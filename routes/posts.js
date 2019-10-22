58663840d42b9de96b8603e76d0d01b4017ab0e3
EC2 Default User <ec2-user@ip-172-31-43-79.us-east-2.compute.internal>
2019-10-22 20:17:07 +0000

    added basic routing and templating code and exercise

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  { 
-      title: 'Jason\' site',
-      message: 'Howdy!'
-  });
+  res.send('respond with all posts');
 });
 
 module.exports = router;
